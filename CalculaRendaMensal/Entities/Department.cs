d7ff9b22c63055f3387fb4e5b058404545e26d6e
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 11:29:00 -0300

    Criacao da classe Department

---
+++ CalculaRendaMensal/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace CalculaRendaMensal.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
--- CalculaRendaMensal/Program.cs
+++ CalculaRendaMensal/Program.cs
@@ -6,7 +6,7 @@ namespace CalculaRendaMensal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
