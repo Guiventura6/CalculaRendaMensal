b0d70444db4f8e90add5b220239df3542ecef123
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 16:53:30 -0300

    Implementacao finalizada

--- CalculaRendaMensal/Program.cs
+++ CalculaRendaMensal/Program.cs
@@ -25,9 +25,7 @@ namespace CalculaRendaMensal
             Department department = new Department(dep);
 
             Worker worker = new Worker(name, level, salary, department);
-
-            //Console.WriteLine($"{worker.Name}, {level}, {salary.ToString("F2", CI)}, {department}");
-
+            
             Console.Write("How many contracts to this worker? ");
             int nContracts = int.Parse(Console.ReadLine());
             for (int i = 1; i <= nContracts; i++)
@@ -41,29 +39,17 @@ namespace CalculaRendaMensal
                 int hours = int.Parse(Console.ReadLine());
 
                 HourContract contract = new HourContract(date, valuePerHour, hours);
-                worker.AddContract(contract);
-
-                //Console.WriteLine($"{hourContract.Date}, {hourContract.ValuePerHour.ToString("F2", CI)}, {hourContract.Hours}");
-                //Console.WriteLine("================================");
-                
+                worker.AddContract(contract);                
             }
+            Console.WriteLine();
 
-
-
-
-
-            /*int cont = 1;
-            foreach (HourContract item in worker.Contracts)
-            {
-                Console.WriteLine($"CONTRACT #{cont}");
-                Console.WriteLine(item.Date);
-                Console.WriteLine(item.ValuePerHour);
-                Console.WriteLine(item.Hours);
-                Console.WriteLine(item.TotalValue());
-                cont++;
-                Console.WriteLine("================================");
-            }*/
-
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            DateTime searchMonth = DateTime.Parse(Console.ReadLine());
+            double income =  worker.Income(searchMonth.Year, searchMonth.Month);
+            
+            Console.WriteLine($"Name: {worker.Name}");
+            Console.WriteLine($"Department: {worker.Department}");
+            Console.WriteLine($"Income for 08/2018: {income.ToString("F2", CI)}");
         }
     }
 }
