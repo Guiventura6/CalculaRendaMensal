28e13f22f2fd173327ab7eb6d98b721836f8b7e3
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 13:09:38 -0300

    Implementado a instaciacao e adicao da lista de contratos no objeto worker

--- CalculaRendaMensal/Program.cs
+++ CalculaRendaMensal/Program.cs
@@ -26,7 +26,43 @@ namespace CalculaRendaMensal
 
             Worker worker = new Worker(name, level, salary, department);
 
-            Console.WriteLine($"{worker.Name}, {level}, {salary.ToString("F2", CI)}, {department}");
+            //Console.WriteLine($"{worker.Name}, {level}, {salary.ToString("F2", CI)}, {department}");
+
+            Console.Write("How many contracts to this worker? ");
+            int nContracts = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= nContracts; i++)
+            {
+                Console.WriteLine($"Enter #{i} contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Duration (hours): ");
+                int hours = int.Parse(Console.ReadLine());
+
+                HourContract contract = new HourContract(date, valuePerHour, hours);
+                worker.AddContract(contract);
+
+                //Console.WriteLine($"{hourContract.Date}, {hourContract.ValuePerHour.ToString("F2", CI)}, {hourContract.Hours}");
+                //Console.WriteLine("================================");
+                
+            }
+
+
+
+
+
+            /*int cont = 1;
+            foreach (HourContract item in worker.Contracts)
+            {
+                Console.WriteLine($"CONTRACT #{cont}");
+                Console.WriteLine(item.Date);
+                Console.WriteLine(item.ValuePerHour);
+                Console.WriteLine(item.Hours);
+                Console.WriteLine(item.TotalValue());
+                cont++;
+                Console.WriteLine("================================");
+            }*/
 
         }
     }
