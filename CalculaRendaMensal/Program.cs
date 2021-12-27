b60477eeb7624a8962fb5addfa038c932dec50e8
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-27 11:05:47 -0300

    Projeto inciado

---
+++ CalculaRendaMensal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculaRendaMensal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
